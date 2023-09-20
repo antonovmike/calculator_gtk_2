df5bd33dc550fb4a754b1af2e632658d7b1ec6e1
antonovmike <antonovma@outlook.com>
2023-09-20 11:10:03 +0400

    refactor calculation.py

--- calculations.py
+++ calculations.py
@@ -1,24 +1,17 @@
 def do_math(content):
     print(content)
-    if [pos for pos, char in enumerate(content) if char == '+']:
-        parts = content.split('+')
-        format_parts = parts[0], "+", parts[1], "=", float(parts[0]) + float(parts[1])
-        str_parts = str(format_parts[0] + format_parts[1] + format_parts[2] + format_parts[3] + str(format_parts[4]))
-        return str_parts
-    elif [pos for pos, char in enumerate(content) if char == '-']:
-        parts = content.split('-')
-        format_parts = parts[0], "-", parts[1], "=", float(parts[0]) - float(parts[1])
-        str_parts = str(format_parts[0] + format_parts[1] + format_parts[2] + format_parts[3] + str(format_parts[4]))
-        return str_parts
-    elif [pos for pos, char in enumerate(content) if char == 'x']:
-        parts = content.split('x')
-        format_parts = parts[0], "x", parts[1], "=", float(parts[0]) * float(parts[1])
-        str_parts = str(format_parts[0] + format_parts[1] + format_parts[2] + format_parts[3] + str(format_parts[4]))
-        return str_parts
-    elif [pos for pos, char in enumerate(content) if char == '/']:
-        parts = content.split('/')
-        format_parts = parts[0], "/", parts[1], "=", float(parts[0]) / float(parts[1])
-        str_parts = str(format_parts[0] + format_parts[1] + format_parts[2] + format_parts[3] + str(format_parts[4]))
-        return str_parts
-    else:
-        print("Operand not found")
+
+    operations = {
+        '+': float.__add__,
+        '-': float.__sub__,
+        'x': float.__mul__,
+        '/': float.__truediv__
+    }
+
+    for operation, function in operations.items():
+        if operation in content:
+            parts = content.split(operation)
+            result = function(float(parts[0]), float(parts[1]))
+            return f"{parts[0]} {operation} {parts[1]} = {result}"
+
+    print("Operand not found")
